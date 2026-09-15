327fe4508eea6aaedefe97e49283ed5cbc1a5095
kateryna-goliak <kateryna.goliak@gmail.com>
2026-09-15 18:53:18 +0300

    remove unnecessary blank line in order submission test

--- tests/test-user_can_successfully_submit_order.spec.ts
+++ tests/test-user_can_successfully_submit_order.spec.ts
@@ -5,7 +5,6 @@ test('user_can_successfully_submit_order', async ({ page }) => {
   await page.locator('[data-test="Espresso_Macchiato"]').click();
   await page.locator('[data-test="Americano"]').click();
   await page.locator('[data-test="checkout"]').click();
-
   await page.getByRole('textbox', { name: 'Name' }).fill('test');
   await page.getByRole('textbox', { name: 'Email' }).fill('test@gmail.com');
   await page.getByRole('button', { name: 'Submit' }).click();
